5f78aa66f5b6f29a04acac3cf03ecf022e019569
JiadingGai <jiading.gai@gmail.com>
2020-07-04 12:57:44 -0400

    Add a diamond like DAG so topo order is well defined.

--- graph_algos/dfs_gai.py
+++ graph_algos/dfs_gai.py
@@ -4,14 +4,25 @@ import matplotlib.pyplot as plt
 
 #g = nx.read_adjlist('./sample.graph')
 g = nx.DiGraph()
-g.add_weighted_edges_from([("u", "v", 0.75), \
-                           ("v", "y", 0.75), \
-                           ("y", "x", 0.75), \
-                           ("u", "x", 0.75), \
-                           ("x", "v", 0.75), \
-                           ("w", "y", 0.75), \
-                           ("w", "z", 0.75), \
-                           ("z", "z", 0.75)  \
+
+# Book example
+#g.add_weighted_edges_from([("u", "v", 0.75), \
+#                           ("v", "y", 0.75), \
+#                           ("y", "x", 0.75), \
+#                           ("u", "x", 0.75), \
+#                           ("x", "v", 0.75), \
+#                           ("w", "y", 0.75), \
+#                           ("w", "z", 0.75), \
+#                           ("z", "z", 0.75)  \
+#                          ])
+
+# Real diamond
+g.add_weighted_edges_from([("u", "x", 0.75), \
+                           ("u", "y", 0.75), \
+                           ("u", "z", 0.75), \
+                           ("x", "y", 0.75), \
+                           ("y", "w", 0.75), \
+                           ("z", "w", 0.75), \
                           ])
 
 # Viz Method 1: use matplotlib
@@ -50,7 +61,7 @@ def DFS_visit_iter(g, color, depth, finish, parent, node):
     time = time + 1
     depth[node] = time
     color[node] = 'Gray'
-    # Using list as a stack: 
+    # Using list as a stack:
     # https://docs.python.org/2/tutorial/datastructures.html#using-lists-as-stacks
     stack = [(node, iter(list(g[node])))] # i.e., (v, adj(v))
 
@@ -61,7 +72,7 @@ def DFS_visit_iter(g, color, depth, finish, parent, node):
         if v is None:
             time = time + 1
             color[cur_node] = 'Black'
-            finish[cur_node] = time 
+            finish[cur_node] = time
             stack.pop()
             continue
 
@@ -75,10 +86,11 @@ def DFS_visit_iter(g, color, depth, finish, parent, node):
 for node in nodes:
     if color[node] == 'White':
         print("Visiting \n - top level", node)
-        #DFS_visit(g, color, depth, finish, parent, node)
-        DFS_visit_iter(g, color, depth, finish, parent, node)
+        DFS_visit(g, color, depth, finish, parent, node)
+        #DFS_visit_iter(g, color, depth, finish, parent, node)
 print(depth)
 print(finish)
+print(parent)
 
 print("\n[Golden Reference]:")
 print("""list(nx.dfs_edges(g, source="u"))""")
@@ -89,6 +101,6 @@ print(list(nx.dfs_tree(g, source="u")))
 
 print("""list(nx.dfs_labeled_edges(g, source="u"))""")
 print(list(nx.dfs_labeled_edges(g, source="u")))
-# Viz Method 2: use graphviz 
+# Viz Method 2: use graphviz
 #h = nx.nx_agraph.from_agraph(g)
 #nx.write_dot(h, './graph.dot')
