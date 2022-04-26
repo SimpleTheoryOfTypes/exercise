91ca83e3c858732462acabcf3f4ae4a6b9274285
Jiading Gai <jiadinggai@Jiadings-MacBook-Air.local>
2022-04-26 16:42:18 -0400

    [LC] Sol236

---
+++ CCOJ/LC/236/Sol236.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isBipartite(self, graph):
+      N = len(graph)
+      for n in range(0, N, 1):
+        if not graph[n]:
+          continue
+        color = {}
+        queue = [n]
+        color[n] = 0
+        while queue:
+            u = queue.pop(0)
+            c = 1 - color[u]
+            for v in graph[u]:
+              if (v in color):
+                if color[v] == color[u]:
+                  return False
+              else:
+                   color[v] = 1 - color[u]
+                   queue.append(v)
+      return True
+
+sol = Solution()
+print(sol.isBipartite([[1,2,3],[0,2],[0,1,3],[0,2]]))
