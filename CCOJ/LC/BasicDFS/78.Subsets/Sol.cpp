b3aed71f8dd2ffc46832f423754a64c1d5ed05ef
Jiading Gai <jiadinggai@Jiadings-MacBook-Air.local>
2022-06-12 16:08:15 -0700

    Fix indentation.

--- CCOJ/LC/BasicDFS/78.Subsets/Sol.cpp
+++ CCOJ/LC/BasicDFS/78.Subsets/Sol.cpp
@@ -6,36 +6,35 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-	    vector<vector<int>> ans;
+        vector<vector<int>> ans;
 
-	    string Nstr = "";
-	    for (int i = 0; i < nums.size(); i++)
-		    Nstr.push_back('1');
+        string Nstr = "";
+        for (int i = 0; i < nums.size(); i++)
+            Nstr.push_back('1');
 
-	    int N = stoi(Nstr, nullptr, 2);
+        int N = stoi(Nstr, nullptr, 2);
 
-            while (N > 0) {
-              string Nstr = int_binary_to_str(N, nums.size());
-	      std::cout << "(JiadingGAI) N = " << N << ", Nstr = "<< Nstr << std::endl;
-	      vector<int> a_subset = {};
-              for (int i = 0; i < nums.size(); i++) {
-		if (Nstr[i] == '1') {
-  		  a_subset.push_back(nums[i]);
-		}
-              }
-	      ans.push_back(a_subset);
-	      print_vector(a_subset);
-              N--;
+        while (N > 0) {
+          string Nstr = int_binary_to_str(N, nums.size());
+          std::cout << "(SimpleTheoryOfTypes) N = " << N << ", Nstr = "<< Nstr << std::endl;
+          vector<int> a_subset = {};
+          for (int i = 0; i < nums.size(); i++) {
+            if (Nstr[i] == '1') {
+              a_subset.push_back(nums[i]);
             }
+          }
+          ans.push_back(a_subset);
+          N--;
+        }
 
-	    ans.push_back(vector<int> {});
-	    return ans;
+        ans.push_back(vector<int> {});
+        return ans;
         
     }
 
     void print_vector(vector<int>& v) {
-	    for (size_t i = 0; i < v.size(); i++)
-		    std::cout << "|" << v[i] << "\n";
+        for (size_t i = 0; i < v.size(); i++)
+            std::cout << "|" << v[i] << "\n";
     }
 
     string int_binary_to_str(unsigned int x, unsigned int nbits) { 
@@ -43,12 +42,12 @@ public:
       unsigned int mask = 1;
       string result;
       for (size_t i = 0; i < nbits; i++) {
-	     unsigned int bit_value = x & mask;
-	     if (bit_value == 0)
-		     result.insert(0, "0");
-	     else
-		     result.insert(0, "1");
-	     x = x >> 1;
+         unsigned int bit_value = x & mask;
+         if (bit_value == 0)
+             result.insert(0, "0");
+         else
+             result.insert(0, "1");
+         x = x >> 1;
       }
       return result;
     }
