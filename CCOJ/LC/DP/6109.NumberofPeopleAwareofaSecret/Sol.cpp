ea2aef25c6b263cf4af848971efebe8b3dd39f95
SimpleTheoryOfTypes <jiadinggai@Jiadings-MacBook-Air.local>
2022-07-04 19:58:16 -0700

    [199.BinaryTreeRightSideView]

--- CCOJ/LC/DP/6109.NumberofPeopleAwareofaSecret/Sol.cpp
+++ CCOJ/LC/DP/6109.NumberofPeopleAwareofaSecret/Sol.cpp
@@ -6,27 +6,18 @@ class Solution {
 public:
     uint64_t peopleAwareOfSecret(int n, int delay, int forget) {
       vector<uint64_t> K(n, 0);//K[i] represents the number of people who know the secret the first time on the ith day.
-      vector<uint64_t> F(n, 0);//F[i] represents the number of people who forget the secret on the ith day.
 
       // Set up initial condition.
       K[0] = 1;
 
       // Iterate till the nth day.
       for (int i = delay; i < n; i++) {
-        if (i - forget >= 0) {
-          // On the ith day, those ppl who know the secret the first time on
-          // the (i-forget)-th day need to forget the secret on the ith day.
-          F[i] = K[i - forget];
-        }
-        // "delay" days includes the counting of the days this person knows
-        // the secret for the first time; therefore the "delay - 1" below.
-        for (int x = (delay - 0); x <= (forget - 1); x++) {
+        for (int x = delay; x < forget; x++) {
           if (i - x >= 0) {
             // On the ith day, everyone who had knew the secret that post-delay,
             // pre-forget can tell the secret to a new person each!
             K[i] += K[i - x] % (1000000000 + 7);
           }
-          //std::cout << "(SimpleTheoryOfTypes) K[" << i << "] = " << K[i] << std::endl;
         }
         K[i] %= (1000000000 + 7);
       }
