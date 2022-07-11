32b04c6b2667271695fd675258c13b7fa3d358b5
SimpleTheoryOfTypes <jiadinggai@Jiadings-MacBook-Air.local>
2022-07-10 18:03:51 -0700

    [33.SearchinRotatedSortedArray] Update a true O(log(n)) solution.

--- CCOJ/LC/BinarySearch/33.SearchinRotatedSortedArray/Sol.cpp
+++ CCOJ/LC/BinarySearch/33.SearchinRotatedSortedArray/Sol.cpp
@@ -4,54 +4,30 @@ using namespace std;
 class Solution {
   public:
     int search(const vector<int>& nums, int target) {
-      if (nums.size() == 1) {
-        if (nums[0] == target)
-          return 0;
-        return -1;
-      }
-
-      int lo = -1;
-      int hi = -1;
-
-      if (nums[0] < nums[nums.size() - 1]) {
-        // The firt element less than the last element.
-        // Not pivoting in the input array.
-        lo = 0;
-        hi = nums.size() - 1;
-      } else {
-        // The input array has pivoting. Identity the pivot
-        // index, and decide which portion of the array we
-        // should search for the target.
-        int maxGap = INT_MIN;
-        int pivotIndex = 0;
-        for (int i = 0; i < nums.size() - 1; i++) {
-          if (maxGap < nums[i] - nums[i + 1]) {
-            maxGap = nums[i] - nums[i + 1];
-            pivotIndex = i;
-          }
-        } 
-
-        if (target >= nums[pivotIndex + 1] && target <= nums[nums.size() - 1]) {
-          // binary search the right portion.
-          lo = pivotIndex + 1;
-          hi = nums.size() - 1;
-        } else {
-          // binary search the left portion.
-          lo = 0;
-          hi = pivotIndex;
-        }
-      }
+      int lo = 0;
+      int hi = nums.size() - 1;
 
       while (lo <= hi) {
         int mid = (lo + hi) / 2;
-        if (nums[mid] > target)
-          hi = mid - 1;
-        else if (nums[mid] < target)
-          lo = mid + 1;
-        else
-          return mid;
-      }
 
+        if (target == nums[mid])
+          return mid;
+
+        // Right portion
+        if (nums[mid] < nums[hi]) {
+          if (target < nums[mid] || target > nums[hi]) {
+            hi = mid - 1;
+          } else {
+            lo = mid + 1;
+          }
+        } else {
+          if (target > nums[mid] || target <= nums[hi]) {
+            lo = mid + 1;
+          } else {
+            hi = mid - 1;
+          }
+        }
+      }
       return -1;
     }
 };
@@ -59,6 +35,6 @@ class Solution {
 int main() {
   auto sol = Solution();
   /* auto ans = sol.search({4,5,6,7,0,1,2}, 0); */
-  auto ans = sol.search({1,3}, 3);
+  auto ans = sol.search({3,1,2}, 3);
   return ans;
 }
